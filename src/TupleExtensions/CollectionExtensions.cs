62f1cebfc6751f5b6cc271ff89136f89df445ee3
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-01-18 14:55:45 +0900

    KeyValuePair.Deconstruct

---
+++ src/TupleExtensions/CollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TupleExtensions
+{
+    /// <summary>
+    /// Extension methods for collections.
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// deconstructor for <see cref="KeyValuePair{TKey, TValue}"/>.
+        /// </summary>
+        public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> p, out TKey key, out TValue value)
+        {
+            key = p.Key;
+            value = p.Value;
+        }
+    }
+}
