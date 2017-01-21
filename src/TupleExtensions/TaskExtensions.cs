a304933e7c5a8f0555b336f19d4b0e52b034b8ae
Nobuyuki Iwanaga <ufcpp@live.jp>
2017-01-21 12:08:27 +0900

    await on tuples / (Task, Task).GetAwaiter

---
+++ src/TupleExtensions/TaskExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace TupleExtensions
+{
+    public static class TaskExtensions
+    {
+        public static TaskAwaiter GetAwaiter(this(Task, Task) t) => Task.WhenAll(t.Item1, t.Item2).GetAwaiter();
+        public static TaskAwaiter GetAwaiter(this(Task, Task, Task) t) => Task.WhenAll(t.Item1, t.Item2, t.Item3).GetAwaiter();
+        public static TaskAwaiter GetAwaiter(this(Task, Task, Task, Task) t) => Task.WhenAll(t.Item1, t.Item2, t.Item3, t.Item4).GetAwaiter();
+        public static TaskAwaiter GetAwaiter(this(Task, Task, Task, Task, Task) t) => Task.WhenAll(t.Item1, t.Item2, t.Item3, t.Item4, t.Item5).GetAwaiter();
+        public static TaskAwaiter GetAwaiter(this(Task, Task, Task, Task, Task, Task) t) => Task.WhenAll(t.Item1, t.Item2, t.Item3, t.Item4, t.Item5, t.Item6).GetAwaiter();
+        public static TaskAwaiter GetAwaiter(this(Task, Task, Task, Task, Task, Task, Task) t) => Task.WhenAll(t.Item1, t.Item2, t.Item3, t.Item4, t.Item5, t.Item6, t.Item7).GetAwaiter();
+        public static TaskAwaiter<TResult[]> GetAwaiter<TResult>(this(Task<TResult>, Task<TResult>) t) => Task.WhenAll(t.Item1, t.Item2).GetAwaiter();
+        public static TaskAwaiter<TResult[]> GetAwaiter<TResult>(this(Task<TResult>, Task<TResult>, Task<TResult>) t) => Task.WhenAll(t.Item1, t.Item2, t.Item3).GetAwaiter();
+        public static TaskAwaiter<TResult[]> GetAwaiter<TResult>(this(Task<TResult>, Task<TResult>, Task<TResult>, Task<TResult>) t) => Task.WhenAll(t.Item1, t.Item2, t.Item3, t.Item4).GetAwaiter();
+        public static TaskAwaiter<TResult[]> GetAwaiter<TResult>(this(Task<TResult>, Task<TResult>, Task<TResult>, Task<TResult>, Task<TResult>) t) => Task.WhenAll(t.Item1, t.Item2, t.Item3, t.Item4, t.Item5).GetAwaiter();
+        public static TaskAwaiter<TResult[]> GetAwaiter<TResult>(this(Task<TResult>, Task<TResult>, Task<TResult>, Task<TResult>, Task<TResult>, Task<TResult>) t) => Task.WhenAll(t.Item1, t.Item2, t.Item3, t.Item4, t.Item5, t.Item6).GetAwaiter();
+        public static TaskAwaiter<TResult[]> GetAwaiter<TResult>(this(Task<TResult>, Task<TResult>, Task<TResult>, Task<TResult>, Task<TResult>, Task<TResult>, Task<TResult>) t) => Task.WhenAll(t.Item1, t.Item2, t.Item3, t.Item4, t.Item5, t.Item6, t.Item7).GetAwaiter();
+    }
+}
